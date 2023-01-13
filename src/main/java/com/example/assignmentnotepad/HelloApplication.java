c88238dae2e1d86383d2b1605375181a7c78cff2
Natnael Deyas <natnaeldeyas0@gmail.com>
2023-01-13 19:44:37 +0300

    Update HelloApplication.java

--- src/main/java/com/example/assignmentnotepad/HelloApplication.java
+++ src/main/java/com/example/assignmentnotepad/HelloApplication.java
@@ -24,11 +24,7 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-//        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-//        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-//        stage.setTitle("Hello!");
-//        stage.setScene(scene);
-//        stage.show();
+
         Label introLabel = new Label("Login to continue");
         introLabel.setAlignment(Pos.CENTER);
         introLabel.setStyle("-fx-color:blue;");
